640ff385a3ff70e6df561b44176416f444625ce5
A-Random-Panda <yaoh667@gmail.com>
2026-08-12 22:12:32 -0700

    added more commands

---
+++ index.js
@@ -0,0 +1,54 @@
+
+
+require("dotenv").config();
+
+const axios = require("axios");
+const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true
+});
+
+app.command("/rpg-ping", async ({ command, ack, respond }) => {
+  const start = Date.now();
+  await ack();
+  const latency = Date.now() - start;
+  await respond({ text: `Pong!\nLatency: ${latency}ms` });
+});
+
+(async () => {
+  await app.start();
+  console.log("bot is running!");
+})();
+
+app.command("/rpg-catfact", async ({ ack, respond }) => {
+  await ack();
+
+  try {
+    const response = await axios.get("https://catfact.ninja/fact");
+    await respond({ text: `Cat Fact:\n${response.data.fact}` });
+  } catch (err) {
+    await respond({ text: "Failed to fetch a cat fact." });
+  }
+});
+
+app.command("/slackrpg-help", async ({ ack, respond }) => {
+  await ack();
+  await respond({
+    text:
+`Available Commands:
+/rpg-ping - Check bot latency
+/rpg-catfact - Get a cat fact`
+  });
+});
+print("ig we should just type code every now and then")
+app.command("/rpg-search", async ({ ack, respond}) => {
+  await ack()
+  await respond({
+    text:
+'You found a wild bore!'
+
+  });
+});
